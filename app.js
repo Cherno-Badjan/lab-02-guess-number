718c3cec714ce5c761b8dd7b1b28f0e5c681bcb2
Cherno Badjan <badjancherno@gmail.com>
2021-01-12 14:05:52 -0800

    DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // import functions and grab DOM elements
+const submitButton = document.getElementById('submit-button');
+const playAgain = document.getElementById('restart');
+const numberGuessed = document.getElementById('user-guess');
+const results = document.getElementById('result');
+
 
 // initialize state
+let randomNumber = Math.ceil(Math.random() * 20);
+let guessesRemaining = 4;
 
 // set event listeners to update state and DOM
+submitButton.addEventListener('click', () => {
+    guessesRemaining--;
+    console.log(guessesRemaining);
+
+})
