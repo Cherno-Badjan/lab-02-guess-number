a923dde89ef2ef4fe13ce7d31a03c3ce9bce9aed
Cherno Badjan <badjancherno@gmail.com>
2021-01-12 21:14:35 -0800

    eliminate guesses display for winner

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { compareNumbers } from "./utils.js";
 
 const submitButton = document.getElementById('submit-button');
-const playAgain = document.getElementById('restart');
+const playAgainButton = document.getElementById('restart');
 const userGuess = document.getElementById('user-guess');
 const results = document.getElementById('result');
 const numberOfGuesses = document.getElementById('guesses');
@@ -11,7 +11,7 @@ const numberOfGuesses = document.getElementById('guesses');
 // initialize state
 let randomNumber = Math.ceil(Math.random() * 20);
 let correctNumber = randomNumber;
-let guessesRemaining = 4;
+let guessesRemaining = 10;
 
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
@@ -19,11 +19,6 @@ submitButton.addEventListener('click', () => {
     let userInput = Number(userGuess.value);
     const answer = compareNumbers(userInput, correctNumber);
 
-    if (guessesRemaining > 0 && answer === 0) {
-
-        numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
-        results.textContent = (`Go play the lotto right now. You are super lucky!`);
-    }
     if (guessesRemaining > 0 && answer === 1) {
 
         numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
@@ -34,10 +29,25 @@ submitButton.addEventListener('click', () => {
         numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
         results.textContent = (`Too low!Try again`);
     }
-    if (guessesRemaining === 0 || guessesRemaining < 0) {
-        numberOfGuesses.textContent = (`You have no guesses left`);
+    if (guessesRemaining === 0) {
+        numberOfGuesses.textContent = (`You have no guesses left. Back to square one`);
+    } else {
+        if (answer === 0) {
+
+            results.textContent = (`You won! Go play the lotto right now. You are super lucky!`);
+            numberOfGuesses.textContent = '';
+        }
     }
 
 
-
 });
+
+/*playAgainButton.addEventListener('click', () => {
+
+
+    guessesRemaining = 10;
+    results.textContent = '';
+    numberOfGuesses.textContent = '';
+    userGuess.textContent = '';
+
+});*/
