75d9d2677b36b7d8a4b565f0f609d0fbc0151e3c
Cherno Badjan <badjancherno@gmail.com>
2021-01-12 16:00:48 -0800

    testing function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions and grab DOM elements
+import { compareNumbers } from "./utils.js";
+
 const submitButton = document.getElementById('submit-button');
 const playAgain = document.getElementById('restart');
 const userGuess = document.getElementById('user-guess');
@@ -8,16 +10,22 @@ const numberOfGuesses = document.getElementById('guesses');
 
 // initialize state
 let randomNumber = Math.ceil(Math.random() * 20);
+let correctNumber = randomNumber;
 let guessesRemaining = 4;
 
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
     guessesRemaining--;
 
-    numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
-    if (guessesRemaining === 0) {
-        numberOfGuesses.textContent = (`No guesses left`);
+    if (guessesRemaining > 0) {
+
+        numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
+    } else if (guessesRemaining === 0 || guessesRemaining < 0) {
+        numberOfGuesses.textContent = (`You have no guesses left`);
     }
 
+    if (compareNumbers(userGuess) === 0) {
+        results.textContent = (`Go play the lotto right now. You are super lucky!`)
+    }
 
-})
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,30 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { compareNumbers } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('compare two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 0;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = compareNumbers(5, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('compare two numbers', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 0;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(5, 6);
 
     //Expect
     // Make assertions about what is expected versus the actual result
