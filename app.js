82581719b5b8469e826bf1753bb06f66435a62a3
Cherno Badjan <badjancherno@gmail.com>
2021-01-16 15:32:22 -0800

    final version

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // import functions and grab DOM elements
-
 import { compareNumbers } from './utils.js';
 
-
 const submitButton = document.getElementById('submit-button');
+const playAgainButton = document.getElementById('restart');
 const userGuess = document.getElementById('user-guess');
 const results = document.getElementById('result');
 const numberOfGuesses = document.getElementById('guesses');
 
+
 // initialize state
-let correctNumber = generateRandomNumber();
+let randomNumber = Math.ceil(Math.random() * 20);
+let correctNumber = randomNumber;
 let guessesRemaining = 4;
+
+console.log(correctNumber);
 
 
 
@@ -19,8 +22,8 @@ submitButton.addEventListener('click', () => {
     guessesRemaining--;
     let userInput = Number(userGuess.value);
     const answer = compareNumbers(userInput, correctNumber);
-    //Evaluates answer (by calling compareNumbers function) and gives and display number of guesses remaining
 
+    //Evaluates answer (by calling compareNumbers function) and gives and display number of guesses remaining
 
     submitFunction(answer);
 });
@@ -42,23 +45,16 @@ playAgainButton.addEventListener('click', () => {
 function submitFunction(answer) {
     if (guessesRemaining > 0 && answer === 1) {
 
-=======
-    
-    if (answer === 1) {
-
         numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
         results.textContent = (`Too high!Try again`);
     }
-    if (answer === -1) {
+    if (guessesRemaining > 0 && answer === -1) {
+
         numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
         results.textContent = (`Too low!Try again`);
     }
-    if (answer === 0) {
-        results.textContent = (`You won! Go play the lotto right now. You are super lucky!`);
-        submitButton.disabled = true;
-        numberOfGuesses.textContent = '';
-    } else if (guessesRemaining === 0 && answer !== 0) {
-        numberOfGuesses.textContent = (`You have no guesses left. Back to square one`);
+    if (guessesRemaining === 0) {
+        numberOfGuesses.textContent = (`You have no guesses left. Back to square one. The correct number was ${correctNumber}.`);
         submitButton.disabled = true;
         results.textContent = '';
     } else {
@@ -70,8 +66,3 @@ function submitFunction(answer) {
         }
     }
 }
-
-=======
-    } 
-});
-
