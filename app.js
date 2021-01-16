da3c60b40e2284bc27f4aa15da453ddc890f93f7
Cherno Badjan <badjancherno@gmail.com>
2021-01-16 13:40:33 -0800

    refactor and reset button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import { compareNumbers } from "./utils.js";
+import { compareNumbers } from './utils.js';
 
 const submitButton = document.getElementById('submit-button');
 const playAgainButton = document.getElementById('restart');
@@ -13,6 +13,8 @@ let randomNumber = Math.ceil(Math.random() * 20);
 let correctNumber = randomNumber;
 let guessesRemaining = 4;
 
+
+
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
     guessesRemaining--;
@@ -21,6 +23,24 @@ submitButton.addEventListener('click', () => {
 
     //Evaluates answer (by calling compareNumbers function) and gives and display number of guesses remaining
 
+    submitFunction(answer);
+});
+
+playAgainButton.addEventListener('click', () => {
+    results.textContent = '';
+    submitButton.disabled = false;
+    userGuess.value = '';
+    randomNumber = Math.ceil(Math.random() * 20);
+    correctNumber = randomNumber;
+    guessesRemaining = 4;
+    numberOfGuesses.textContent = '';
+
+
+});
+
+
+
+function submitFunction(answer) {
     if (guessesRemaining > 0 && answer === 1) {
 
         numberOfGuesses.textContent = (`You have ${guessesRemaining} guesses left!`);
@@ -43,7 +63,5 @@ submitButton.addEventListener('click', () => {
             numberOfGuesses.textContent = '';
         }
     }
-});
-
-
+}
 
